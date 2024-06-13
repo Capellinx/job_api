3f1b345dbefcc8fa98584d8b96d6ad4cdd5f5107
Capellinx <capellaaa7@gmail.com>
2024-06-13 19:42:13 -0300

    feat: implementando middlewares nas rotas

--- src/routes/application.route.ts
+++ src/routes/application.route.ts
@@ -1,9 +1,14 @@
 import { Router } from "express";
 import { ApplicationController } from "../controllers/application.controller";
+import { ValidateBody } from "../middlewares";
+import { applicationCreateSchema } from "../schemas/application.schema";
 
 export const applicationRouter = Router()
 
 const applicationController = new ApplicationController()
 
-applicationRouter.post("/:id/applications", applicationController.create)
+applicationRouter.post("/:id/applications",
+   ValidateBody.execute(applicationCreateSchema),
+   applicationController.create
+);
 applicationRouter.get("/:id/applications", applicationController.findMany)
--- src/routes/opportunity.route.ts
+++ src/routes/opportunity.route.ts
@@ -1,15 +1,24 @@
 import { Router } from "express";
 import { OpportunityControllers } from "../controllers/opportunity.controller";
 import { applicationRouter } from "./application.route";
+import { ValidateBody } from "../middlewares";
+import { opportunityCreateSchema } from "../schemas/opportunity.schema";
 
-export const opportunityRouter = Router()
+export const opportunityRouter = Router();
 
-const opportunityController = new OpportunityControllers()
+const opportunityController = new OpportunityControllers();
 
-opportunityRouter.post("/", opportunityController.create)
-opportunityRouter.get("/", opportunityController.findMany)
-opportunityRouter.get("/:id", opportunityController.findOne)
-opportunityRouter.patch("/:id", opportunityController.update)
-opportunityRouter.delete("/:id", opportunityController.delete)
+opportunityRouter.post("/",
+   ValidateBody.execute(opportunityCreateSchema),
+   opportunityController.create
+);
+opportunityRouter.get("/", opportunityController.findMany);
+opportunityRouter.get("/:id", opportunityController.findOne);
 
-opportunityRouter.use("/", applicationRouter)
+opportunityRouter.patch("/:id",
+   ValidateBody.execute(opportunityCreateSchema),
+   opportunityController.update
+);
+opportunityRouter.delete("/:id", opportunityController.delete);
+
+opportunityRouter.use("/", applicationRouter);
