a16bb467cf595668e6b59f845e3a72eb08531cf2
Capellinx <capellaaa7@gmail.com>
2024-06-13 16:55:32 -0300

    feat: criando controllers

---
+++ src/controllers/application.controller.ts
@@ -0,0 +1,11 @@
+import { Request, Response } from "express";
+
+export class ApplicationController {
+   public create(req: Request, res: Response) {
+
+   };
+   
+   public findMany(req: Request, res: Response) {
+
+   };
+};
---
+++ src/controllers/opportunity.controller.ts
@@ -0,0 +1,23 @@
+import { Request, Response } from "express";
+
+export class OpportunityControllers {
+   public create(req: Request, res: Response) {
+
+   };
+
+   public findMany(req: Request, res: Response) {
+
+   };
+
+   public findOne(req: Request, res: Response) {
+
+   };
+
+   public update(req: Request, res: Response) {
+
+   };
+   
+   public delete(req: Request, res: Response) {
+
+   };
+};
