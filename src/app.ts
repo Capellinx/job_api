12d447183dd7cb4a1923de9d908af10b0a2d636c
Capellinx <capellaaa7@gmail.com>
2024-06-13 18:19:26 -0300

    feat: adicionano helmet e rota para uso

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,12 @@
 
+import { opportunityRouter } from "./routes";
+
 import express, { json } from "express";
-import helmet from "helmet"
+import helmet from "helmet";
 
 export const app = express();
 
-app.use(helmet())
-app.use(json())
+app.use(helmet());
+app.use(json());
+
+app.use("/opportunities", opportunityRouter);
