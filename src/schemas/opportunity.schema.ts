65a87073b959c57bae72173bc91e7d780e62b8ff
Capellinx <capellaaa7@gmail.com>
2024-06-13 18:45:41 -0300

    feat: criando novas tipagem apartir do schema

--- src/schemas/opportunity.schema.ts
+++ src/schemas/opportunity.schema.ts
@@ -4,9 +4,11 @@ export const opportunitySchema = z.object({
    id: z.number().int().positive(),
    title: z.string().min(1).max(255),
    description: z.string().min(1)
-})
+});
 
-export const opportunityCreateSchema = opportunitySchema.omit({ id: true })
+export const opportunityCreateSchema = opportunitySchema.omit({ id: true });
+export const opportunityUpdateSchema = opportunityCreateSchema.partial();
 
-export type TopportunityCreate = z.infer<typeof opportunityCreateSchema>
-export type TOpportunity = z.infer<typeof opportunitySchema>
+export type TopportunityCreate = z.infer<typeof opportunityCreateSchema>;
+export type TOpportunity = z.infer<typeof opportunitySchema>;
+export type TOpportunityUpdate = z.infer<typeof opportunityUpdateSchema>;
