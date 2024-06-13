153a71e7ea0a992d2fa3b7298edf05891b4d23a5
Capellinx <capellaaa7@gmail.com>
2024-06-13 18:51:23 -0300

    fix: colocando id na rota de deleção

--- src/routes/opportunity.route.ts
+++ src/routes/opportunity.route.ts
@@ -10,6 +10,6 @@ opportunityRouter.post("/", opportunityController.create)
 opportunityRouter.get("/", opportunityController.findMany)
 opportunityRouter.get("/:id", opportunityController.findOne)
 opportunityRouter.patch("/:id", opportunityController.update)
-opportunityRouter.delete("/", opportunityController.delete)
+opportunityRouter.delete("/:id", opportunityController.delete)
 
 opportunityRouter.use("/:id/applications", applicationRouter)
