70e9b185ae898779c342269f712926ef4e5e917f
Capellinx <capellaaa7@gmail.com>
2024-06-13 19:19:29 -0300

    feat: fazendo conexão com o serviço de application

--- src/controllers/application.controller.ts
+++ src/controllers/application.controller.ts
@@ -1,11 +1,18 @@
 import { Request, Response } from "express";
+import { ApplicationServices } from "../services";
 
 export class ApplicationController {
-   public create(req: Request, res: Response) {
+   private applicationService = new ApplicationServices()
 
+   create = async (req: Request, res: Response) => {
+      const response = await this.applicationService.create(Number(req.params.id), req.body);
+
+      return res.status(201).json(response)
    };
-   
-   public findMany(req: Request, res: Response) {
 
+   findMany = async (req: Request, res: Response) => {
+      const response = await this.applicationService.findMany(Number(req.params.id));
+
+      return res.status(200).json(response)
    };
 };
