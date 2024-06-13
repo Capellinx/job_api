cf687aad4ca42bba9b95aa3dd7bf592e6bf375cc
Capellinx <capellaaa7@gmail.com>
2024-06-13 18:35:44 -0300

    feat: criando serviço de de listar por id e listar todos

--- src/services/opportunity.service.ts
+++ src/services/opportunity.service.ts
@@ -1,21 +1,28 @@
 import { prisma } from "../../prisma/prisma";
 import { TOpportunity, TopportunityCreate } from "../schemas";
-
 export class OpportunityServices {
    async create(body: TopportunityCreate): Promise<TOpportunity> {
       const data = await prisma.oppotunity.create({ data: body });
 
       return data;
    };
-   async findMany() {
 
+   async findMany(): Promise<TOpportunity[]> {
+      const data = await prisma.oppotunity.findMany();
+      
+      return data;
    };
-   async findOne() {
 
+   async findOne(id: number): Promise<TOpportunity> {
+      const data = await prisma.oppotunity.findUnique({ where: { id: id } });
+
+      return data as TOpportunity;
    };
+
    async update() {
 
    };
+
    async delete() {
 
    };
